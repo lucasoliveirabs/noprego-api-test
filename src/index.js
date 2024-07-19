b5e0ed2aeae40f86dd0f672039ef24f5e779e0db
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-07-19 13:51:03 -0300

    add diff port

--- src/index.js
+++ src/index.js
@@ -25,11 +25,9 @@ const httpsServer = https.createServer(options, app);
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("MongoDB connection success");
-    httpsServer.listen(PORT, '0.0.0.0', () => {
+    httpsServer.listen(PORT, '192.168.1.100', () => {
         console.log("Server listening at port "+PORT);
     });
 }).catch(() => {
     console.log("MongoDB connection failed")
-}).catch(() => {
-    console.log("MongoDB connection failed");
 })
