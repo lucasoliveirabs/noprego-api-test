fa5fdfa3d9d0b81ee7706cc0432b637e0639e5f5
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-07-19 13:18:35 -0300

    feat: get('/')

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import https from 'https';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Running API');
+});
+
+const options = {
+    key: fs.readFileSync('./localhost-key.pem', 'utf8'),
+    cert: fs.readFileSync('./localhost.pem', 'utf8')
+};
+const httpsServer = https.createServer(options, app);
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("MongoDB connection success");
+    httpsServer.listen(PORT, () => {
+        console.log("Server listening at port "+PORT);
+    });
+}).catch(() => {
+    console.log("MongoDB connection failed")
+}).catch(() => {
+    console.log("MongoDB connection failed");
+})
