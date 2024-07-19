214274e749cbd9483299f534d51eda69e1b9392a
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-07-19 17:09:39 -0300

    back to http

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express'
 import cors from 'cors';
-import fs from 'fs';
 import mongoose from 'mongoose';
-import https from 'https';
-import http from 'http';
-
 
 const app = express();
 app.use(cors());
@@ -18,24 +14,13 @@ app.get('/', (req, res) => {
     res.send('Running API');
 });
 
-/*const options = {
-    key: fs.readFileSync('./localhost-key.pem', 'utf8'),
-    cert: fs.readFileSync('./localhost.pem', 'utf8')
-};
-const httpsServer = https.createServer(options, app);*/
-
-app.listen(PORT, function(err){
-    if (err) console.log("Error in server setup");
-    console.log("Server listening on Port", PORT);
-})
-
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("MongoDB connection success");
-    /*
-    httpsServer.listen(PORT, '0.0.0.0', () => {
-        console.log("Server listening at port "+PORT);
-    });*/
+    app.listen(PORT, function(err){
+        if (err) console.log("Error in server setup");
+        console.log("Server listening on Port", PORT);
+    })
 }).catch(() => {
     console.log("MongoDB connection failed")
 })
