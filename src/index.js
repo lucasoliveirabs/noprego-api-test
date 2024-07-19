4d359ecfa6293a719318376cc31855e5151f5904
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-07-19 13:26:17 -0300

    import dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv';
+import express from 'express'
+import cors from 'cors';
 import fs from 'fs';
 import https from 'https';
-import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
+
+const PORT = isNaN(parseInt(process.env.PORT)) ? 3000 : parseInt(process.env.PORT); 
 
 app.get('/', (req, res) => {
     res.send('Running API');
